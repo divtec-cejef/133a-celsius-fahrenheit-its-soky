b8345bb9dc259eb899e5ab2c962d63bb5502e016
its-soky <118964160+its-soky@users.noreply.github.com>
2023-09-05 15:05:44 +0200

    Convertisseur C° en F°

--- js/main.js
+++ js/main.js
@@ -1,11 +1,14 @@
 /**
  * Fichier de base pour application web HTML/CSS/JS
- * @author  Steve Fallet
- * @version 0.2
- * @since   2019-08-19
+ * @author  Djovensky
+ * @version 0.1
+ * @since   05.09.2023
  */
-'use strict'; // Demande un interprétation stricte du code
-
-let a = 'Bonjour';
-
-console.log(a);
+"use strict"; // Demande un interprétation stricte du code
+let celsius = parseInt(prompt("Rentrez une température en C°"));
+if (isNaN(celsius) === false){
+    let fahrenheit = celsius * (9 / 5) + 32;
+    alert(fahrenheit + " F°");
+} else {
+    alert("Veuillez mettre un nombre.")
+}
